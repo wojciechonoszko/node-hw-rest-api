0024a2c9c8f0a651d4be757e471f02e095943c87
Wojciech <wojciechonoszko@gmail.com>
2022-08-26 14:31:45 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -51,12 +51,40 @@ router.post('/', validateCreateContact, async (req, res, next) => {
   
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+//DELETE
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const contacts = await Contacts.remove(req.params.id);
+
+    if (contacts) {
+      return res.status(200).json({
+        status: 'success',
+        code: 200,
+        message: 'contact deleted',
+      });
+    }
+    return res
+    .status(404)
+    .json({ status: 'error', code: 404, message: 'Not Found' });
+  } catch (error) {
+    next(error);
+  }
 })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+//PUT
+router.put('/:id', ValidateUpdateContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.update(req.params.id, req.body);
+
+    if (contact) {
+      return res.status(200).json({ status: 'success', code: 200, contact });
+    }
+    return res
+    .status(404)
+    .json({ status: 'error', code: 404, message: 'Not Found'});
+  } catch (error){
+    next(error);
+  }
 })
 
 module.exports = router
