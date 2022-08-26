1de99404d3276ad2aaa48c62c90e6baf78231531
Wojciech <wojciechonoszko@gmail.com>
2022-08-26 18:23:04 +0200

    cange

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require('./routes/api/contacts');
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.;json())
 
-app.use('/api/contacts', contactsRouter)
-app.use('/api/contacts/:contactId', contactsRouter)
+app.use('/api/contacts', contactsRouter);
+app.use('/api/contacts/:id', contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ status: 'error', code: 404, message: 'Not found' })
@@ -23,6 +23,6 @@ app.use((err, req, res, next) => {
   const code = err.status || 500;
   const status = err.status ? 'error' : 'fail';
   res.status(500).json({ status, code, message: err.message })
-})
+});
 
 module.exports = app
