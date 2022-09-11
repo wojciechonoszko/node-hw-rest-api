69c93b5074730bf9b15d99433c52ccbe62e704bc
Wojciech <wojciechonoszko@gmail.com>
2022-09-11 12:55:46 +0200

    email changes

--- models/schemas/user.js
+++ models/schemas/user.js
@@ -28,6 +28,14 @@ const userSchema = new Schema({
         type: String,
         required: [true, "Avatar is required"],
       },
+      verify: {
+        type: Boolean,
+        default: false,
+      },
+      verificationToken: {
+        type: String,
+        required: [true, 'Verify token is required'],
+      },
 });
 
 userSchema.methods.setPassword = async function (password) {
