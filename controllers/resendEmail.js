c0b259b1999455049bc849b75f4dd8aa9d03a7e1
Wojciech <wojciechonoszko@gmail.com>
2022-09-12 13:00:35 +0200

    finish changes

--- controllers/resendEmail.js
+++ controllers/resendEmail.js
@@ -2,24 +2,26 @@ const { User } = require('../models/schemas/user');
 const { HttpCode } = require('../helpers/constants');
 const shortFunc = require('../models/shortFunctions');
 const {sendEmail} = require ('../helpers/sendEmail');
-const { handle } = require ('./verifyEmail.js');
+// const  handle  = require ('./verifyEmail.js');
+const { v4 } = require("uuid");
 
 const resendEmail = async (req, res, next) => {
 
     try {
         const { email } = req.body;
+        const verificationToken = v4();
 
         if (!email) {
             res.json({
                 status: 'error',
                 code: HttpCode.BAD_REQUEST,
                 ResponseBody: { 
-                    message: "Missing required email", 
+                    message: "Missing required field email", 
         },
             })
         return;
         }
-        const user = await Users.findByEmail(req.body.email);
+        const user = await shortFunc.findByEmail(email);
 
         if (!user){
             res.json({
@@ -42,7 +44,13 @@ const resendEmail = async (req, res, next) => {
         return;
         }
 
-        const result = await handle.verifyEmail(email, user.verifyToken);
+        // const result = await handle.sendEmail(email, verificationToken);
+        const mail = {
+            to: email,
+            subject: "Confirm your email again, please.",
+            html: `a href="http://localhost:8155/api/users/verify/:${verificationToken}" target="_blank">Click to confirm email</a>`,
+        };
+        await sendEmail(mail);
         res.json({
             status: 'success',
             code: HttpCode.OK,
@@ -51,9 +59,7 @@ const resendEmail = async (req, res, next) => {
           });
         } catch (error) {
           next(error);
-        }
-
-    
+        }  
 };
 
 module.exports = { resendEmail };
