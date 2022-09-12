377783f6742b1776c582223ead24ccad75fbd4fb
Wojciech <wojciechonoszko@gmail.com>
2022-09-12 08:59:44 +0200

    Update resendEmail.js

--- controllers/resendEmail.js
+++ controllers/resendEmail.js
@@ -1,4 +1,57 @@
 const { User } = require('../models/schemas/user');
 const { HttpCode } = require('../helpers/constants');
+const shortFunc = require('../models/shortFunctions');
+const {sendEmail} = require ('../../helpers/sendEmail');
+const { handle } = require ('./verifyEmail.js');
 
-const
+const resendEmail = async (req, res, next) => {
+
+    try {
+        const { email } = req.body;
+
+        if (!email) {
+            res.json({
+                status: 'error',
+                code: HttpCode.BAD_REQUEST,
+                ResponseBody: { 
+                    message: "Missing required email", 
+        },
+            })
+        return;
+        }
+        const user = await Users.findByEmail(req.body.email);
+
+        if (!user){
+            res.json({
+                status: 'error',
+                code: HttpCode.BAD_REQUEST,
+                ResponseBody: { 
+                    message: "User with this email is not found", 
+        },
+            })
+        return;
+        }
+        if (user.verify) {
+            res.json({
+                status: 'error',
+                code: HttpCode.BAD_REQUEST,
+                ResponseBody: { 
+                    message: "Verification has already been passed", 
+        },
+            })
+        return;
+        }
+
+        const result = await handle.verifyEmail(email, user.verifyToken);
+        res.json({
+            status: 'success',
+            code: HttpCode.OK,
+            ResponseBody: {
+                message: 'Verification email sent'}
+          });
+        } catch (error) {
+          next(error);
+        }
+
+    }
+}
